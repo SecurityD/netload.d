b43463ec00ebe201d2bcfd7bc83155ba04c4e152
Alan Zanatta <alan.zanatta@epitech.eu>
2015-06-24 19:12:21 +0200

    Adds base for snmpv1

---
+++ source/netload/protocols/snmp/snmptrapv1.d
@@ -0,0 +1,31 @@
+module netload.protocols.snmp.trapv1;
+
+import netload.core.protocol;
+import vibe.data.json;
+
+class SNMPTrapv1 : Protocol {
+  public:
+    @property Protocol data() {
+
+    }
+
+    void prepare() {
+
+    }
+
+    Json toJson() {
+
+    }
+
+    ubyte[] toBytes() {
+
+    }
+
+    string toString() {
+
+    }
+
+  private:
+
+
+}
---
+++ source/netload/protocols/snmp/snmpv1.d
@@ -0,0 +1,32 @@
+module netload.protocols.snmp.v1;
+
+import netload.core.protocol;
+import vibe.data.json;
+
+class SNMPv1 : Protocol {
+  public:
+    @property Protocol data() {
+
+    }
+
+    void prepare() {
+
+    }
+
+    Json toJson() {
+
+    }
+
+    ubyte[] toBytes() {
+
+    }
+
+    string toString() {
+
+    }
+
+  private:
+    int _version;
+    string _community;
+
+}
