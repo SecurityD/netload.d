85340f53052b94d5aa68ccfb14f42f0ca56d9837
Alan Zanatta <alan.zanatta@epitech.eu>
2015-06-27 17:56:13 +0200

    Adds base code for ARP module

---
+++ source/netload/protocols/arp/arp.d
@@ -0,0 +1,58 @@
+module netload.protocols.arp;
+
+import netload.core.protocol;
+import vibe.data.json;
+
+class ARP : Protocol {
+  public:
+    @property Protocol data() {
+
+    }
+
+    void prepare() {
+
+    }
+
+    Json toJson() {
+
+    }
+
+    unittest {
+
+    }
+
+    ubyte[] toBytes() {
+
+    }
+
+    unittest {
+
+    }
+
+    override string toString() {
+
+    }
+
+    unittest {
+
+    }
+    
+  private:
+
+}
+
+ARP toARP(Json json) {
+
+}
+
+unittest  {
+
+}
+
+ARP toARP(ubyte[] encoded) {
+
+}
+
+unittest {
+
+}
