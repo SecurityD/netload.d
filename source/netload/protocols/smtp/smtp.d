5057fff9d3fd2b1154145c8ea5006b4b3a4c4df2
Matthieu Kern <matthieu.kern@epitech.eu>
2015-07-01 16:29:39 +0200

    Add SMTP

---
+++ source/netload/protocols/smtp/smtp.d
@@ -0,0 +1,76 @@
+module netload.protocols.smtp;
+
+import vibe.data.json;
+import netload.core.protocol;
+
+class SMTP : Protocol {
+  public:
+    this() {
+
+    }
+
+    this(string b) {
+      _body = b;
+    }
+
+    override @property inout string name() { return "SMTP"; };
+    @disable override @property Protocol data() { return null; }
+    override @property void data(Protocol p) { }
+    override @property int osiLayer() const { return 7; }
+
+    override Json toJson() const {
+      Json json = Json.emptyObject;
+      json.body_ = _body;
+      return json;
+    }
+
+    unittest {
+      SMTP packet = new SMTP("test");
+      auto json = Json.emptyObject;
+      json.body_ = "test";
+      assert(packet.toJson == json);
+    }
+
+    override ubyte[] toBytes() const {
+      return cast(ubyte[])(_body.dup);
+    }
+
+    unittest {
+      SMTP packet = new SMTP("test");
+      assert(packet.toBytes == cast(ubyte[])("test"));
+    }
+
+    override string toString() const {
+      return toJson.toString;
+    }
+
+    @property string str() const { return _body; }
+    @property void str(string b) { _body = b; }
+
+  private:
+    string _body;
+}
+
+SMTP toSMTP(Json json) {
+  SMTP packet = new SMTP();
+  packet.str = json.body_.to!string;
+  return packet;
+}
+
+unittest {
+  Json json = Json.emptyObject;
+  json.body_ = "test";
+  SMTP packet = toSMTP(json);
+  assert(packet.str == "test");
+}
+
+SMTP toSMTP(ubyte[] encoded) {
+  SMTP packet = new SMTP(cast(string)(encoded));
+  return packet;
+}
+
+unittest {
+  ubyte[] encoded = [116, 101, 115, 116];
+  SMTP packet = encoded.toSMTP();
+  assert(packet.str == "test");
+}
